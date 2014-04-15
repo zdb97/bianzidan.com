8b0a2c8737820774a7c79cd0d509a591d9a05488
Zidan Bian <zidan.bian@optus.com.au>
2014-04-15 15:46:27 +1000

    add foundation gem

---
+++ compass_conf.rb
@@ -0,0 +1,5 @@
+# official documentation of the compass config file: http://compass-style.org/help/tutorials/configuration-reference/
+
+# plugin requirements
+# Require any additional compass plugins? Uncomment the following line
+require "zurb-foundation"
--- gruntfile.js
+++ gruntfile.js
@@ -2,29 +2,32 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+
     pkg: grunt.file.readJSON('package.json'),
+
 	compass: {                  // Task
 		prod: {                   // Target
-		  options: {              // Target options
-			sassDir: 'scss',
-			cssDir: 'css',
-			environment: 'production',
-			outputStyle: 'compressed',
-			noLineComments: true
-		  }
+		  	options: {              // Target options
+		  		config: 'compass_conf.rb',
+				sassDir: 'scss',
+				cssDir: 'css',
+				environment: 'production',
+				outputStyle: 'compressed',
+				noLineComments: true
+		  	}
 		},
 		dev: {                    // Another target
-		  options: {
-			sassDir: 'scss',
-			cssDir: 'css',
-			environment: 'development',
-			outputStyle: 'compact',
-			noLineComments: false
-		  }
+		  	options: {
+		  		config: 'compass_conf.rb',
+				sassDir: 'scss',
+				cssDir: 'css',
+				environment: 'development',
+				outputStyle: 'compact',
+				noLineComments: false
+		  	}
 		}
-	  }
-  
-	 
+  	}
+
   });
 
   // Load the plugin that provides the "uglify" task.
